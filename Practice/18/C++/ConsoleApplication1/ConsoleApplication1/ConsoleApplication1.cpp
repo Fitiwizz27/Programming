4af541f88ae26fe20cbabb59a4f3d929f1686c26
Fitiwizz <fitiwizz27@mail.ru>
2020-10-17 16:49:30 +0300

    Programming

---
+++ Practice/18/C++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int a = 26, ver, b;
+	string s1;
+
+	cin >> s1;
+
+	while (s1[0] == true) {
+
+	}
+
+	system("pause");
+	return(0);
+}
