aff415ce37f3a858d8275212d709ae95f6f2c79c
Fitiwizz <fitiwizz27@mail.ru>
2020-10-25 17:07:17 +0300

    Programming

--- Practice/18/C++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Practice/18/C++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,17 +8,34 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	int z, len;
-	string c;
+	string slovo;
+	double chanse = 1;
+	int len = 0;
+	char bykvy[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'x'};
 	vector<string> s1 = {"hallo", "klempner", "das", "ist", "fantastisch", "fluggegecheimen"};
-	char letters[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-	
+	double letters[26] = {};
+
 	for (int i = 0; i < s1.size(); i++) {
 		for (int j = 0; j < s1[i].size(); j++) {
-			cout << s1[i][j];
+			len++;
+			for (int k = 0; k < 26; k++) {
+				if (s1[i][j] == bykvy[k]) letters[k]++;
+			}
 		}
-		cout << " ";
 	}
+
+	for (int i = 0; i < 26; i++) letters[i] = letters[i] / len;
+
+	cin >> slovo;
+
+	for (int i = 0; i < slovo.size(); i++) {
+		for (int j = 0; j < 26; j++) {
+			if (slovo[i] == bykvy[j]) chanse = chanse * letters[j];
+		}
+	}
+
+	cout.precision(16);
+	cout << chanse << endl;
 
 	system("pause");
 	return(0);
