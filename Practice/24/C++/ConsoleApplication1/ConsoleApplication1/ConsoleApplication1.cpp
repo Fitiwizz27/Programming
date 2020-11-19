744f3a01106c83aef7af5c2dbdbc32dd86c9c5ad
Fitiwizz <fitiwizz27@mail.ru>
2020-11-19 22:29:23 +0300

    Programming

---
+++ Practice/24/C++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include "json.hpp"
+#include <fstream>
+#include <iomanip>
+
+using namespace nlohmann;
+using namespace std;
+
+int main()
+{
+    json j;
+    ifstream input("in.json");
+    input >> j;
+
+    json o;
+    ofstream output("out.json");
+
+    for (auto& x : j)
+    {
+        int  userId = x["userId"];
+        bool completed = x["completed"];
+
+        json a;
+        json* user = &a;
+
+        for (auto& element : o)
+        {
+            if (element["userId"] == userId)
+            {
+                user = &element;
+                break;
+            }
+        }
+
+        if (user->empty())
+        {
+            o.push_back({ {"userId", userId}, {"task_completed", 0} });
+        }
+
+        user = &o[o.size() - 1];
+
+        if (completed)
+        {
+            (*user)["task_completed"] = static_cast<int>((*user)["task_completed"]) + 1;
+        }
+    }
+
+    output << setw(4) << o << endl;
+    return(0);
+}
